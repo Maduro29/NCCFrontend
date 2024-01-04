6b64e90d7f03e74b0ba30f9056306b729074c58d
Maduro29 <manhdungtran.vnuuet@gmail.com>
2024-01-05 03:40:25 +0700

    responsive done

--- src/component/NavItem/NavItem.js
+++ src/component/NavItem/NavItem.js
@@ -3,10 +3,10 @@ import './NavItem.css'
 
 const NavItem = (props) => {
 
-    const { text, active, home } = props;
+    const { text, active, home, more } = props;
 
     return (
-        <div className={`navitem ${active ? 'active' : ''} ${home ? 'home' : ''}`}>
+        <div className={`navitem ${active ? 'active' : ''} ${home ? 'home' : ''} ${more ? 'more' : ''}`}>
             <span className='navitem-text'>
                 <a href='#'>{text}</a>
             </span>
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -34,9 +34,12 @@ const Navbar = () => {
 
   return (
     <div className='navbar'>
+      <NavItem text={'More'} active={false} home={true} more='more'/>
+      <div className='navbar-box'>
       {navData.map(data => {
         return <NavItem text={data.text} active={data.active} home={data.home}/>
       })}
+      </div>
     </div>
   )
 }
